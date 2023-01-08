8929a7c6b25fcfc111b9a4c5d338cf9bc3aea8b4
ElizavetaAlferova <lalferova202@gmail.com>
2023-01-08 20:49:26 +0300

    Initial commit

---
+++ neighboring dates.py
@@ -0,0 +1,19 @@
+'''
+На вход программе подается последовательность дат, разделенных пробелом, в формате DD.MM.YYYY.
+
+Формат выходных данных
+Программа должна вывести список, содержащий неотрицательные целые числа,
+ каждое из которых — количество дней между двумя соседними датами последовательности.
+
+'''
+
+from datetime import datetime, timedelta, date
+sample='%d.%m.%Y'
+diff_neighbor=[]
+
+list_dates=[datetime.strptime(i, sample) for i in input().split()]
+for i in range(len(list_dates)-1):
+    diff_neighbor.append(abs(list_dates[i]-list_dates[i+1]).days)
+
+
+print(diff_neighbor)
