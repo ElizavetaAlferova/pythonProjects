ac6cb8318860924948d936f0a77ac426d4daef8c
ElizavetaAlferova <lalferova202@gmail.com>
2023-01-09 20:55:34 +0300

    Fibonashe def

--- fibonache.py
+++ fibonache.py
@@ -1,9 +1,9 @@
 def fib(n):
     my_list = [i for i in range(0, n + 1)]
-    fib=[0, 1, 1]
-    if n>2:
-        for i in range(3, n+1):
-            fib.append(fib[i-1]+fib[i-2])
+    fib = [0, 1, 1]
+    if n > 2:
+        for i in range(3, n + 1):
+            fib.append(fib[i - 1] + fib[i - 2])
 
     return fib[n]
 
