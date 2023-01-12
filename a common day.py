a2cf2299fa3e587893d1fade393d3b3a92b79ee7
ElizavetaAlferova <lalferova202@gmail.com>
2023-01-12 20:52:30 +0300

    commit

---
+++ a common day.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+pattern = '%d.%m.%Y'
+n = int(input())
+list_of_customers = []
+for i in range(n):
+    list_of_customers.append(datetime.strptime(input().split()[2], pattern))
+# print(list_of_customers)
+frequency = {}
+for i in list_of_customers:
+    frequency[i] = frequency.get(i, 0) + 1
+# print(frequency)
+
+new = sorted(frequency.items(), key=lambda item: item[1], reverse=True)
+common = new[0][1]
+result = []
+for i in frequency:
+    if frequency[i] == common:
+        result.append(i)
+
+# print(new)
+# print(common)
+result = sorted(result)
+for i in result:
+    print(datetime.strftime(i, pattern))
+
+# for i in list_of_customers:
